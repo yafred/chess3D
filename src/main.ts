1e1f7852db339192dc959a61dbff09dff14a2735
YaFred <yafred@users.noreply.github.com>
2026-03-28 20:52:32 +0100

    fix imports

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import * as THREE from 'three';
-import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { fenToScene } from './fen';
 
 const scene = new THREE.Scene();
