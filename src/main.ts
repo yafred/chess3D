bc422f48a28dd2f64307681ffeb3b667f9de4f11
YaFred <yafred@users.noreply.github.com>
2026-03-29 19:46:11 +0200

    tweak camera

--- src/main.ts
+++ src/main.ts
@@ -64,7 +64,7 @@ loader.load(sceneAssetUrl, (gltf) => {
 
 // Camera
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100);
-camera.position.set(0, 8, 10);
+camera.position.set(0, 15, 8);
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
