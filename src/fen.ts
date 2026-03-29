bb0429a3978821be138afb79c078a277a53dc44f
YaFred <yafred@users.noreply.github.com>
2026-03-29 14:22:53 +0200

    clean up clones before displaying a new fen

--- src/fen.ts
+++ src/fen.ts
@@ -9,6 +9,14 @@ const pieceMap: { [key: string]: string } = {
     'K': 'King'};
     
 export function fenToScene(fen: string, scene: THREE.Scene, pieces: Map<string, THREE.Mesh>, materials: Map<string, THREE.Material>) {
+    // Remove clones previously created.
+    for (let i = scene.children.length - 1; i >= 0; i--) {
+        const child = scene.children[i];
+        if (child.userData?.isFenClone) {
+            scene.remove(child);
+        }
+    }
+
     const rows = fen.split(' ')[0].split('/');
     for (let r = 0; r < 8; r++) {
         let c = 0;
@@ -20,6 +28,7 @@ export function fenToScene(fen: string, scene: THREE.Scene, pieces: Map<string, 
     
                 if (pieceMesh) {
                     const clone = pieceMesh.clone();
+                    clone.userData.isFenClone = true;
                     clone.position.set(c - 3.5, 0, r - 3.5);
                     clone.name = `${char}`;  // Name the piece for later reference (e.g., "P" for white pawn, "p" for black pawn)
                     // Reminder: X: horizontal positive to the right, Y: vertical positive up, Z: horizontal positive towards the camera    
