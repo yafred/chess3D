d52b6bcc0891bbf3f8051100971bee1527855000
YaFred <yafred@users.noreply.github.com>
2026-03-29 20:49:01 +0200

    tweak initial camera

--- src/main.ts
+++ src/main.ts
@@ -65,6 +65,8 @@ loader.load(sceneAssetUrl, (gltf) => {
 // Camera
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 100);
 camera.position.set(0, 15, 8);
+camera.zoom = 1.5;
+camera.updateProjectionMatrix();
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({ antialias: true });
